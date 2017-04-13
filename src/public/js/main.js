5c76dbd77e22d89d39baffdf5a72ad268ff427a4
Alex Lane <alex.lane@internetrti.com>
2017-04-13 11:45:34 -0400

    Fixed navigation for small device

---
+++ src/public/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var li = ($('#bs-example-navbar-collapse-1 ul li');
+
+    console.log(li);
+
+
+    if($('#bs-example-navbar-collapse-1 ul li').length = 0) {
+        $('#collapse-button').hide();
+    }
+});
